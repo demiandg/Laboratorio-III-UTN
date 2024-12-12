c795cc43e86aa098292e5a200a7031d2d7c78728
Damian Di Giovanni <demian-dg@hotmail.com>
2024-12-11 22:05:16 -0300

    Solución Act 2.1

--- 03-LAB3-Actividad_2.1.sql
+++ 03-LAB3-Actividad_2.1.sql
@@ -1,40 +1,81 @@
 Use Univ
 go
-/*
--- 01 Listado de todos los idiomas.
+
+-- 01 
 select Nombre from Idiomas
 
--- 02 Listado de todos los cursos.
+-- 02 
 select Nombre from Cursos
 
--- 03 Listado con nombre, costo de inscripción (costo de curso), costo de certificación y fecha de estreno de todos los cursos.
+-- 03
 select nombre, CostoCurso as "Costo de inscripción", CostoCertificacion as "Costo de certificación", Estreno as "Fecha de estreno" from Cursos
 
--- 04 Listado con ID, nombre, costo de inscripción y ID de nivel de todos los cursos cuyo costo de certificación sea menor a $5000.
+-- 04 
 select ID, Nombre, CostoCurso, IDNivel from Cursos 
 where CostoCertificacion > 5000
 
--- 05 Listado con ID, nombre, costo de inscripción y ID de nivel de todos los cursos cuyo costo de certificación sea mayor a $1200.
+-- 05 
 select id, Nombre, CostoCurso, IDNivel from Cursos
 where CostoCertificacion < 1200
 
--- 06 Listado con nombre, número y duración de todas las clases del curso con ID número 6.
+-- 06 
 select Nombre, Numero, Duracion from Clases
 where IDCurso = 6
 
--- 07 Listado con nombre, número y duración de todas las clases del curso con ID número 10.
+-- 07 
 select Nombre, Numero, Duracion from Clases
 where IDCurso = 10
 
--- 08 Listado con nombre y duración de todas las clases del curso con ID número 4. Ordenado por duración de mayor a menor.
+-- 08 
 select nombre, Duracion from Clases
 where IDCurso = 4 
 order by Duracion desc
 
--- 09 Listado de cursos con nombre, fecha de estreno, costo del curso, costo de certificación ordenados por fecha de estreno de manera creciente.
+-- 09 
 select Nombre, Estreno as 'fecha de estreno', CostoCurso as 'costo del curso', CostoCertificacion as 'costo de certificación' from Cursos
 order by Estreno 
 
--- 10 Listado con nombre, fecha de estreno y costo del curso de todos aquellos cuyo ID de nivel sea 1, 5, 9 o 10.
+-- 10
 select nombre, Estreno, CostoCurso from Cursos
-where IDNivel in (1,5,9,10)*/
+where IDNivel in (1,5,9,10)
+
+-- 11 
+select top(3) Nombre, Estreno, CostoCurso from Cursos
+order by CostoCertificacion desc
+
+-- 12
+select Nombre, Duracion, Numero from Clases
+where IDCurso in (2,5,7)
+order by IDCurso asc, Numero asc
+
+-- 13 
+select Nombre, Estreno from Cursos
+where Estreno between '2019-01-01' and '2019-05-31'
+
+-- 14
+select Nombre from Cursos
+where year(Estreno) = 2020 
+
+-- 15
+select nombre from Cursos
+where month(Estreno) between 1 and 4 
+
+-- 16 
+select Nombre from Clases
+where Duracion between 15 and 90 
+
+-- 17 
+select * from Contenidos
+where (Tamaño > 5000 and IDTipo = 4) or (Tamaño < 400 and IDTipo = 1) 
+
+-- 18
+select * from Cursos
+where IDNivel is null 
+
+-- 19
+select Nombre from Cursos
+where (0.2*CostoCurso) < CostoCertificacion
+
+-- 20 
+select distinct(CostoCurso) from Cursos
+order by CostoCurso desc
