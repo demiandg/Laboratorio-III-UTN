1cfa6d97c2a3bdde56714c835d5740d4b88e3dde
Damian Di Giovanni <demian-dg@hotmail.com>
2025-01-09 19:51:39 -0300

    Finaliza Guia 2.2

--- 04-LAB3-Actividad 2.2.sql
+++ 04-LAB3-Actividad 2.2.sql
@@ -69,6 +69,7 @@ inner join Usuarios as us on ins.IDUsuario = us.ID
 inner join Datos_Personales as dp on us.ID = dp.ID 
 
 -- 09 Listado de cursos con nombre, costo de cursado y certificación, costo total (cursado + certificación) con 10% de todos los cursos de nivel Principiante.
+
 select cu.Nombre as Curso, cu.CostoCurso, cu.CostoCertificacion, (cu.CostoCurso + cu.CostoCertificacion)*1.1 as 'Costo Total' from Cursos as cu
 inner join Niveles as ni on cu.IDNivel = ni.ID
 where ni.Nombre like 'principiante' 
@@ -76,9 +77,93 @@ where ni.Nombre like 'principiante'
 -- 10 Listado con nombre y apellido y mail de todos los instructores. Sin repetir.
 
 select distinct(dp.Nombres + ' ' +  dp.Apellidos) as Nombre, dp.Email from Instructores_x_Curso as ixc
-inner join Datos_Personales as dp on ixc.IDUsuario = dp.ID */
+inner join Datos_Personales as dp on ixc.IDUsuario = dp.ID 
+
+-- 11 Listado con nombre y apellido de todos los usuarios que hayan cursado algún curso cuya categoría sea 'Historia'.
+
+Select dp.nombres, dp.Apellidos from Datos_Personales as dp
+inner join Usuarios as us on dp.ID = us.ID
+inner join Inscripciones as ins on us.ID = ins.ID
+inner join Cursos as cu on ins.IDCurso = cu.ID
+inner join Categorias_x_Curso as caxcu on cu.ID = caxcu.IDCurso
+inner join Categorias as cat on caxcu.IDCategoria = cat.ID 
+where cat.Nombre like 'Historia' 
+
+-- 12 Listado con nombre de idioma, código de curso y código de tipo de idioma. Listar todos los idiomas indistintamente si no tiene cursos relacionados.
+
+select leng.Nombre, ixc.IDCurso, fi.ID from Idiomas as leng
+left join Idiomas_x_Curso as ixc on leng.id = ixc.IDIdioma
+left join FormatosIdioma as fi on ixc.IDFormatoIdioma = fi.ID 
+
+-- 13 Listado con nombre de idioma de todos los idiomas que no tienen cursos relacionados.
+
+select Nombre from Idiomas as leng
+left join Idiomas_x_Curso as ixc on leng.ID = ixc.IDIdioma
+where ixc.IDCurso is null 
+
+-- 14 Listado con nombres de idioma que figuren como audio de algún curso. Sin repeticiones.
+
+select distinct(leng.Nombre) from Idiomas as leng
+inner join Idiomas_x_Curso as ixc on leng.ID = ixc.IDIdioma
+inner join FormatosIdioma as fi on ixc.IDFormatoIdioma = fi.ID
+where fi.Nombre like 'audio' 
+
+-- 15 Listado con nombres y apellidos de todos los usuarios y el nombre del país en el que nacieron. 
+--	  Listar todos los países indistintamente si no tiene usuarios relacionados.
+
+select dp.Nombres, dp.Apellidos, pa.Nombre from Datos_Personales as dp
+inner join Localidades as loc on dp.IDLocalidad = loc.ID
+right join Paises as pa on loc.IDPais = Pa.ID 
+
+-- 16 Listado con nombre de curso, fecha de estreno y nombres de usuario de todos los inscriptos. 
+--    Listar todos los nombres de usuario indistintamente si no se inscribieron a ningún curso.
+
+select cu.Nombre, cu.Estreno, us.NombreUsuario from Cursos as cu
+inner join Inscripciones as ins on cu.ID = ins.IDCurso
+right join Usuarios as us on ins.IDUsuario = us.ID 
+
+-- 17 Listado con nombre de usuario, apellido, nombres, género, fecha de nacimiento y mail de todos los usuarios que no cursaron ningún curso.
+
+select us.NombreUsuario, dp.Apellidos, dp.Nombres, dp.Genero, dp.Nacimiento, dp.Email from Usuarios as us
+inner join Datos_Personales as dp on us.ID = dp.ID
+left join Inscripciones as ins on us.ID = ins.IDUsuario
+where ins.ID is null 
+
+-- 18 Listado con nombre y apellido, nombre del curso, puntaje otorgado y texto de la reseña. 
+--    Sólo de aquellos usuarios que hayan realizado una reseña inapropiada.
+
+select dp.Nombres, dp.Apellidos, cu.Nombre, re.Puntaje, re.Observaciones from Datos_Personales as dp
+inner join Inscripciones as ins on dp.ID = ins.IDUsuario
+inner join Cursos as cu on ins.IDCurso = cu.ID
+inner join Reseñas as re on ins.ID = re.IDInscripcion
+where re.Inapropiada = 1 
 
 
+-- 19 Listado con nombre del curso, costo de cursado, costo de certificación, nombre del idioma 
+--    y nombre del tipo de idioma de todos los cursos cuya fecha de estreno haya sido antes del año actual.
+--	  Ordenado por nombre del curso y luego por nombre de tipo de idioma. Ambos ascendentemente.
 
+select cu.Nombre, cu.CostoCurso, cu.CostoCertificacion, leng.Nombre as Idioma, fo.Nombre as 'Tipo de Idioma' from Cursos as cu
+left join Idiomas_x_Curso as ixc on cu.ID = ixc.IDCurso
+left join Idiomas as leng on ixc.IDIdioma = leng.ID
+left join FormatosIdioma as fo on ixc.IDFormatoIdioma = fo.ID
+where year(cu.Estreno) = year(getdate())
+order by cu.Nombre, leng.Nombre 
 
+-- 20 Listado con nombre del curso y todos los importes de los pagos relacionados.
+
+select cu.Nombre, concat('$', pa.Importe) from Cursos as cu
+left join Inscripciones as ins on cu.ID = ins.IDCurso
+left join Pagos as pa on ins.ID = pa.IDInscripcion */
+
+-- 21 Listado con nombre de curso, costo de cursado y una leyenda que indique "Costoso" si el costo de cursado es mayor a $ 15000, "Accesible" 
+--    si el costo de cursado está entre $2500 y $15000, "Barato" si el costo está entre $1 y $2499 y "Gratis" si el costo es $0.
+
+Select Nombre as Cusos, CostoCurso as Costo, Observacion = case
+when CostoCurso > 15000 then 'Costoso'
+when CostoCurso between 2500 and 15000 then 'Accesible'
+when CostoCurso between 1 and 1499 then 'Barato'
+else 'Gratis'
+end 
+from Cursos
 
